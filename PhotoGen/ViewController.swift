0d29593d67000d50f97d14d5bfdbd7b9d6a8dd90
andricolae <andreicalutiu@gmail.com>
2023-05-05 11:40:31 +0300

    Initial Commit

---
+++ PhotoGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGen
+//
+//  Created by Andrei-Nicolae Calutiu on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
